28e4860328271176583254e0a3d5a5f2198ae8e1
SUPHASIN <fusesuphasin@gmail.com>
2020-12-16 14:44:28 +0700

    ทำ Entity สำหรับเก็บข้อมูล activities - fix #17

---
+++ backend/ent/schema/activities.go
@@ -0,0 +1,24 @@
+package schema
+
+import (
+	"github.com/facebook/ent"
+)
+
+// Activities holds the schema definition for the Activities entity.
+type Activities struct {
+	ent.Schema
+}
+
+// Fields of the Activities.
+func (Activities) Fields() []ent.Field {
+	return []ent.Field{
+}
+
+// Edges of the Activities.
+func (Activities) Edges() []ent.Edge {
+	return []ent.Edge{
+	}
+}
+
+
+
