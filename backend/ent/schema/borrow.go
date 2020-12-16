46f67ffd30d49eefddb1bab3eb7eadef7fb71828
unknown <thanawatpromkul@gmail.com>
2020-12-16 16:24:23 +0700

    ทำ Entity สำหรับเก็บข้อมูล borrow #25

---
+++ backend/ent/schema/borrow.go
@@ -0,0 +1,28 @@
+package schema
+
+import (
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/field"
+	"github.com/facebook/ent/schema/edge"
+	"time"
+
+)
+// Borrow holds the schema definition for the Borrow entity.
+type Borrow struct {
+	ent.Schema
+}
+
+// Fields of the Borrow.
+func (Borrow) Fields() []ent.Field {
+	return []ent.Field{
+		field.Time("date").Default(time.Now).Immutable(),
+	}
+}
+
+// Edges of the Borrow.
+func (Borrow) Edges() []ent.Edge {
+	return []ent.Edge{
+		
+	}
+}
+
