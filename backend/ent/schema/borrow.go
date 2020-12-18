cd159fe73ed6e21f30c6aa459d491e4c90678685
unknown <thanawatpromkul@gmail.com>
2020-12-18 17:22:15 +0700

    ทำ Entity สำหรับเก็บข้อมูลของ borrow -fix #34

--- backend/ent/schema/borrow.go
+++ backend/ent/schema/borrow.go
@@ -1,21 +1,12 @@
 package schema
 
 import (
- issue-27
-	"github.com/facebook/ent"
-	"github.com/facebook/ent/schema/field"
-	"github.com/facebook/ent/schema/edge"
-	"time"
-
-)
-
 	"time"
 
 	"github.com/facebook/ent"
 	"github.com/facebook/ent/schema/field"
 )
 
- main
 // Borrow holds the schema definition for the Borrow entity.
 type Borrow struct {
 	ent.Schema
@@ -30,12 +21,5 @@ func (Borrow) Fields() []ent.Field {
 
 // Edges of the Borrow.
 func (Borrow) Edges() []ent.Edge {
- issue-27
-	return []ent.Edge{
-		
-	}
-}
-
-
 	return []ent.Edge{}
-} main
+}
