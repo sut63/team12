cc0929b427fa9e39708059b8f90cda4f5b2d99ac
Suphasin Yosang <63347672+fusesuphasin@users.noreply.github.com>
2020-12-20 11:54:31 +0700

    Update activities.go

--- backend/ent/schema/activities.go
+++ backend/ent/schema/activities.go
@@ -2,6 +2,8 @@ package schema
 
 import (
 	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/edge"
+	"github.com/facebook/ent/schema/field"
 )
 
 // Activities holds the schema definition for the Activities entity.
@@ -12,13 +14,17 @@ type Activities struct {
 // Fields of the Activities.
 func (Activities) Fields() []ent.Field {
 	return []ent.Field{
+		field.String("name").NotEmpty(),
+		field.String("detail").NotEmpty(),
+		field.Time("starttime"),
+		field.Time("endtime"),
+	}
 }
 
 // Edges of the Activities.
 func (Activities) Edges() []ent.Edge {
 	return []ent.Edge{
+		edge.From("activitytype", ActivityType.Type).Ref("activities").Unique(),
+		edge.From("academicyear", AcademicYear.Type).Ref("activities").Unique(),
 	}
 }
-
-
-
