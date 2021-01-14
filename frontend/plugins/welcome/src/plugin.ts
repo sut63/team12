d36f016014ef488d5f10c6908a7d6b53afb508d6
SUPHASIN <fusesuphasin@gmail.com>
2021-01-14 17:33:03 +0700

    เพิ่ม router ของ ActivitiTable และ Activities -fix #98

--- frontend/plugins/welcome/src/plugin.ts
+++ frontend/plugins/welcome/src/plugin.ts
@@ -2,6 +2,7 @@ import { createPlugin } from '@backstage/core';
 import WelcomePage from './components/WelcomePage';
 import WatchVideo from './components/WatchVideo';
 import SignIn from './components/SignIn';
+import ActivityTable from './components/ActivityTable';
 import Activities from './components/Activities';
 
 export const plugin = createPlugin({
@@ -11,5 +12,6 @@ export const plugin = createPlugin({
     router.registerRoute('/watch_video', WatchVideo);
     router.registerRoute('/signin', SignIn);
     router.registerRoute('/Activities', Activities);
+    router.registerRoute('/ActivityTable', ActivityTable);
   },
 });
