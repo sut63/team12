ec6315b98bdcb464b0b98a75143b022976d29295
Suphasin Yosang <63347672+fusesuphasin@users.noreply.github.com>
2021-01-14 17:39:12 +0700

    Update plugin.ts

--- frontend/plugins/welcome/src/plugin.ts
+++ frontend/plugins/welcome/src/plugin.ts
@@ -1,15 +1,17 @@
 import { createPlugin } from '@backstage/core';
 import WelcomePage from './components/WelcomePage';
 import WatchVideo from './components/WatchVideo';
-import SignIn from './components/SingIn';
+import SignIn from './components/SignIn';
 import Activities from './components/Activities';
+import ActivityTable from './components/ActivityTable';
 
 export const plugin = createPlugin({
   id: 'welcome',
   register({ router }) {
-    router.registerRoute('/welcome', WelcomePage);
+    router.registerRoute('/', WelcomePage);
     router.registerRoute('/watch_video', WatchVideo);
-    router.registerRoute('/', SignIn);
+    router.registerRoute('/signin', SignIn);
     router.registerRoute('/Activities', Activities);
+    router.registerRoute('/ActivityTable', ActivityTable);
   },
 });
