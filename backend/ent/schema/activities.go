6467dc33d6c67b2dceef73da5aa49a9392d134e3
SUPHASIN <fusesuphasin@gmail.com>
2021-01-21 21:18:56 +0700

    เพิ่ม field location และเพิ่ม Validators ให้กับ field ใน activities schema - fix #204

--- backend/ent/schema/activities.go
+++ backend/ent/schema/activities.go
@@ -1,6 +1,9 @@
 package schema
 
 import (
+	"errors"
+	"regexp"
+
 	"github.com/facebookincubator/ent"
 	"github.com/facebookincubator/ent/schema/edge"
 	"github.com/facebookincubator/ent/schema/field"
@@ -14,8 +17,27 @@ type Activities struct {
 // Fields of the Activities.
 func (Activities) Fields() []ent.Field {
 	return []ent.Field{
-		field.String("name").NotEmpty(),
-		field.String("detail").NotEmpty(),
+		field.String("name").Validate(func(s string) error {
+			match, _ := regexp.MatchString("^[A-Z][A-Za-z_]{6,50}$", s)
+			if !match {
+				return errors.New("รูปแบบชื่อกิจกรรมไม่ถูกต้อง")
+			}
+			return nil
+		}),
+		field.String("detail").Validate(func(s string) error {
+			match, _ := regexp.MatchString(".{49,}", s)
+			if !match {
+				return errors.New("รูปแบบรายละเอียดกิจกรรมไม่ถูกต้อง")
+			}
+			return nil
+		}),
+		field.String("location").Validate(func(s string) error {
+			match, _ := regexp.MatchString("^[A-Z][A-Za-z_0-9]{2,49}$", s)
+			if !match {
+				return errors.New("รูปแบบชื่อสถานที่จัดกิจกรรมไม่ถูกต้อง")
+			}
+			return nil
+		}),
 		field.Time("starttime"),
 		field.Time("endtime"),
 	}
