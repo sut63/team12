5b66ebe3eab3c1accc4c4c7a1ff443bf50b477c2
bluepsm <burapa9826@gmail.com>
2021-02-06 01:23:00 +0700

    ทำ UI ของระบบค้นหาเรื่องร้องเรียน - close #257

--- frontend/plugins/welcome/src/plugin.ts
+++ frontend/plugins/welcome/src/plugin.ts
@@ -1,12 +1,12 @@
 import { createPlugin } from '@backstage/core';
 import WelcomePage from './components/WelcomePage';
-import WatchVideo from './components/WatchVideo';
 import SignIn from './components/SignIn';
 import Activities from './components/Activities';
 import ActivityTable from './components/ActivityTable';
 import Club from './components/club';
 import ClubApplication from './components/ClubApplication';
 import Complaint from './components/Complaint';
+import ComplaintSearch from './components/ComplaintSearch';
 import Roomuse from './components/Roomuse';
 import ClubappSearch from './components/ClubappSearch';
 
@@ -20,6 +20,7 @@ export const plugin = createPlugin({
     router.registerRoute('/Roomuse', Roomuse);
     router.registerRoute('/ActivityTable', ActivityTable);
     router.registerRoute('/Complaint', Complaint);
+    router.registerRoute('/ComplaintSearch', ComplaintSearch);
     router.registerRoute('/Activities', Activities);
     router.registerRoute('/ClubappSearch', ClubappSearch);
   },
